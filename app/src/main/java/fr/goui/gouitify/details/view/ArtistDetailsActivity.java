7334a69f93ab8cc0bb1a0c00cdd41c88c723829d
Galliano Guillaume <guillaume.gallianobondurand.ext@parrot.com>
2016-05-10 14:10:57 +0200

    Added details activities and their layouts.

---
+++ app/src/main/java/fr/goui/gouitify/details/view/ArtistDetailsActivity.java
@@ -0,0 +1,15 @@
+package fr.goui.gouitify.details.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.goui.gouitify.R;
+
+public class ArtistDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_details);
+    }
+}
